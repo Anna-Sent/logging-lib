2b6ee2ea3be24dbc8b9032b459c3d2fe80e61ec3
Anna Sentyakova <asentyakova@65apps.com>
2020-02-13 16:10:20 +0400

    Connected with Firebase, Crashlytics

--- app/src/main/java/io/logging/sample/App.java
+++ app/src/main/java/io/logging/sample/App.java
@@ -3,16 +3,20 @@ package io.logging.sample;
 import android.app.Application;
 import android.content.Context;
 
+import com.crashlytics.android.Crashlytics;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
+import io.fabric.sdk.android.Fabric;
 import io.logging.CrashReportSystem;
 import io.logging.DumpingUncaughtExceptionHandler;
 import io.logging.LogSystem;
 import io.logging.utils.DeviceUtils;
 
+@SuppressWarnings("WeakerAccess")
 public class App extends Application {
 
     private Logger logger;
@@ -54,17 +58,19 @@ public class App extends Application {
                 new CrashReportSystem() {
                     @Override
                     public void init(Context appContext, boolean enableCrs) {
-
+                        if (BuildConfig.ENABLE_CRS) {
+                            Fabric.with(appContext, new Crashlytics());
+                        }
                     }
 
                     @Override
                     public void report(Throwable throwable) {
-
+                        Crashlytics.logException(throwable);
                     }
 
                     @Override
                     public void log(String msg) {
-
+                        Crashlytics.log(msg);
                     }
                 });
         logger = LoggerFactory.getLogger(toString());
--- app/src/main/java/io/logging/sample/MainActivity.java
+++ app/src/main/java/io/logging/sample/MainActivity.java
@@ -1,9 +1,13 @@
 package io.logging.sample;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import com.crashlytics.android.Crashlytics;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,5 +23,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Crashlytics.getInstance().crash();
+            }
+        });
     }
 }
